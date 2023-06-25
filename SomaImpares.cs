06040be28bcf8897f3724c5c96d9ca2a218cf88e
Juliana Góes Bello <91296525+Juuhgb@users.noreply.github.com>
2023-06-24 23:58:55 -0300

    Add files via upload

---
+++ SomaImpares.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+          int a, b, menor, soma;
+
+            Console.WriteLine("Digite dois numeros:");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b) {
+                menor = b;
+                b = a;
+                a = menor;
+            }
+            soma = 0;
+            for (int i = a + 1; i < b; i++) {
+                if (i % 2 != 0){
+                   soma = soma + i; 
+                }
+            }
+            Console.WriteLine("Soma dos impares: " + soma);
+
+        }
+    }
+}
+
+
