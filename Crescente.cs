eaba72198b2e30e69d359b56302505bf1553a26c
Juliana Góes Bello <91296525+Juuhgb@users.noreply.github.com>
2023-06-25 00:08:06 -0300

    Add files via upload

---
+++ Crescente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace crescente
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int a, b;
+
+			Console.WriteLine("Digite dois numeros:");
+			a = Convert.ToInt32(Console.ReadLine());
+			b = Convert.ToInt32(Console.ReadLine());
+
+			while (a != b)
+			{
+				if (a > b)
+				{
+					Console.WriteLine("Decrescente");
+				}
+				else
+				{
+					Console.WriteLine("Crescente");
+				}
+
+				Console.WriteLine("Digite outros dois numeros:");
+				a = Convert.ToInt32(Console.ReadLine());
+				b = Convert.ToInt32
+                (Console.ReadLine());
+			}
+		}
+	}
+}
+
