9fffe2ed492a4aad27190d863fb2a9d788d51ccd
Juliana Góes Bello <91296525+Juuhgb@users.noreply.github.com>
2023-06-25 01:18:05 -0300

    Add files via upload

---
+++ MatrizDiagonal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int N, negativo;
+
+            Console.Write("Qual eh a ordem da matriz? ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mat = new int[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("ELEMENTO[" + i + "," + j + "]:");
+                    mat[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            Console.WriteLine();
+
+            negativo = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        negativo++;
+                    }
+                }
+            }
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS: " + negativo);
+        }
+    }
+}
+
+
